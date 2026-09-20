48d1d2faf117177b03ae9439344109885eb2dd2c
SharkPlush <larryisbetter@proton.me>
2026-09-21 01:35:15 +0300

    Update intel-park.sh

--- intel-park.sh
+++ intel-park.sh
@@ -11,48 +11,6 @@
 
 set -euo pipefail
 
-apply_park_fun() {
-    case $POWER_STATE in
-        power-saver)
-            if ! printf '%s' "$P_CORES" > /sys/fs/cgroup/parked-cores/cpuset.cpus; then
-                return 1
-            fi
-            if ! printf '%s' "$P_CORES" > /sys/fs/cgroup/parked-cores/cpuset.cpus.exclusive; then
-                return 1
-            fi
-            if ! printf 'isolated' > /sys/fs/cgroup/parked-cores/cpuset.cpus.partition; then
-                return 1
-            fi
-            ;;
-        *)
-            # If BALANCED_P_CORES is 0 then P cores will not be used in balanced mode ->
-            # If it is 1 then P cores will be used in balanced mode.
-            if [ "$POWER_STATE" = "balanced" ] && [ "$BALANCED_P_CORES" = "0" ]; then
-                if ! printf '%s' "$P_CORES" > /sys/fs/cgroup/parked-cores/cpuset.cpus; then
-                    return 1
-                fi
-                if ! printf '%s' "$P_CORES" > /sys/fs/cgroup/parked-cores/cpuset.cpus.exclusive; then
-                    return 1
-                fi
-                if ! printf 'isolated' > /sys/fs/cgroup/parked-cores/cpuset.cpus.partition; then
-                    return 1
-                fi
-            else
-                if ! printf 'member' > /sys/fs/cgroup/parked-cores/cpuset.cpus.partition; then
-                    return 1
-                fi
-                if ! printf '%s' "$A_CORES" > /sys/fs/cgroup/parked-cores/cpuset.cpus; then
-                    return 1
-                fi
-                if ! printf '%s' "$A_CORES" > /sys/fs/cgroup/parked-cores/cpuset.cpus.exclusive; then
-                    return 1
-                fi
-            fi
-            ;;
-    esac
-    return 0
-}
-
 # --- ENTRY POINT ---
 if [ -e "/tmp/intel-park.lock" ]; then
     printf 'Another instance of intel-park.sh is already running.\n'
@@ -74,19 +32,6 @@ case $CPU_GEN in
         ;;
 esac
 
-# Variable for controlling if the balanced power profile should have P cores utilized.
-: "${BALANCED_P_CORES:=0}"
-readonly BALANCED_P_CORES
-case $BALANCED_P_CORES in
-    0|1)
-        ;;
-    *)
-        printf 'The BALANCED_P_CORES variable can only be 0 or 1.\n'
-        rm "/tmp/intel-park.lock"
-        exit 2
-        ;;
-esac
-
 # We don't ever park E and LPE cores so we don't need to find them individually.
 # Parking E cores causes power inefficency and parking LPE cores is just not a good idea.
 P_CORES="$(< /sys/devices/cpu_core/cpus)"
@@ -94,8 +39,7 @@ readonly P_CORES
 A_CORES="$(< /sys/devices/system/cpu/present)"
 readonly A_CORES
 
-BUSCTL_OUT=""
-POWER_STATE=""
+HINT=""
 
 # Allows us to actually enable core parking.
 if ! printf '+cpuset\n' > /sys/fs/cgroup/cgroup.subtree_control; then
@@ -110,34 +54,52 @@ if ! mkdir -p '/sys/fs/cgroup/parked-cores'; then
 fi
 
 # If the script exits allow all the cores.
-trap 'rm "/tmp/intel-park.lock"; POWER_STATE="performance"; apply_park_fun' EXIT
+trap 'rmdir "/sys/fs/cgroup/parked-cores"' EXIT
 
 # Before the main loop we should know the current power state the device is in and apply for that.
-if ! POWER_STATE="$(busctl --system get-property org.freedesktop.UPower.PowerProfiles /org/freedesktop/UPower/PowerProfiles org.freedesktop.UPower.PowerProfiles ActiveProfile | grep -m1 -oE "power-saver|balanced|performance")"; then
+if ! HINT="$(< /sys/bus/pci/devices/0000:00:04.0/workload_hint/workload_type_index)"; then
     printf 'Failed to capture power profile state when starting script.\n'
     exit 1
 fi
-if ! apply_park_fun; then
-     printf 'Failed to adjust parked CPU cores.\n'
-     exit 1
-fi
 
-while true; do
-    # busctl listens for a power state changed.
-    # Becauuse this is a listener and not polling extra battery won't be wasted.
-    if ! BUSCTL_OUT="$(busctl --system wait org.freedesktop.UPower.PowerProfiles /org/freedesktop/UPower/PowerProfiles org.freedesktop.DBus.Properties PropertiesChanged)"; then
-        printf "Failed to start busctl listener.\n"
-        exit 1
-    fi
-    grep -q "ActiveProfile" <<<"$BUSCTL_OUT" || continue
-
-    if ! POWER_STATE="$(busctl --system get-property org.freedesktop.UPower.PowerProfiles /org/freedesktop/UPower/PowerProfiles org.freedesktop.UPower.PowerProfiles ActiveProfile | grep -m1 -oE "power-saver|balanced|performance")"; then
-        printf "Failed to capture power profile state.\n"
-        exit 1
-    fi
-
-    if ! apply_park_fun; then
-         printf "Failed to adjust parked CPU cores.\n"
-         exit 1
-    fi
+inotifywait -m -q -e modify /sys/bus/pci/devices/0000:00:04.0/workload_hint/workload_type_index | while read -r _; do
+    HINT="$(< /sys/bus/pci/devices/0000:00:04.0/workload_hint/workload_type_index)"
+    case $HINT in
+        0)
+            if ! printf '%s' "$P_CORES" > /sys/fs/cgroup/parked-cores/cpuset.cpus; then
+                exit 1
+            fi
+            if ! printf '%s' "$P_CORES" > /sys/fs/cgroup/parked-cores/cpuset.cpus.exclusive; then
+                exit 1
+            fi
+            if ! printf 'isolated' > /sys/fs/cgroup/parked-cores/cpuset.cpus.partition; then
+                exit 1
+            fi
+            printf 'idle-ps-test.\n'
+            ;;
+        1|2)
+            if ! printf '%s' "$P_CORES" > /sys/fs/cgroup/parked-cores/cpuset.cpus; then
+                exit 1
+            fi
+            if ! printf '%s' "$P_CORES" > /sys/fs/cgroup/parked-cores/cpuset.cpus.exclusive; then
+                exit 1
+            fi
+            if ! printf 'isolated' > /sys/fs/cgroup/parked-cores/cpuset.cpus.partition; then
+                exit 1
+            fi
+            printf 'active-ps-test.\n'
+            ;;
+        3)
+            if ! printf 'member' > /sys/fs/cgroup/parked-cores/cpuset.cpus.partition; then
+                exit 1
+            fi
+            printf 'sustained-ld-test.\n'
+            ;;
+        *)
+            if ! printf 'member' > /sys/fs/cgroup/parked-cores/cpuset.cpus.partition; then
+                exit 1
+            fi
+            printf 'performance-ld-test.\n'
+            ;;
+    esac
 done
